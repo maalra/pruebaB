301625d919df7980eb4d482c5c287bd313602e7f
Marc Alapont Raga <maalra01@floridauniversitaria.es>
2016-10-19 12:35:31 +0200

    second commit

---
+++ claseordenador.php
@@ -0,0 +1,17 @@
+<?
+class ordenador.php
+{
+  //variables
+  public $marca="apple ";
+
+public function setmarca($cambiarmarca){
+  $this ->marca=$cambiarmarca;}
+  public function getmarca(){
+    return $this ->marca;
+  }
+}
+
+
+
+
+ ?>
